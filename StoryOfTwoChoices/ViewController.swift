42710f994e21fa14fbd8b1ffcfed0cca17f0849f
ttt-net <tenten102matsuyama@gmail.com>
2022-06-09 00:38:16 +0900

    Initial Commit

---
+++ StoryOfTwoChoices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryOfTwoChoices
+//
+//  Created by tetsuta matsuyama on 2022/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
