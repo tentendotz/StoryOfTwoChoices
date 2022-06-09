42e2a54a0abda28c0946499f53fada0759d5eb92
ttt-net <tenten102matsuyama@gmail.com>
2022-06-10 00:08:41 +0900

    Add IBOutlet and IBAction

--- StoryOfTwoChoices/ViewController.swift
+++ StoryOfTwoChoices/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
 
 }
 
